0af99634b0f7855753e8e33543f4eaf62537d937
Unknown <stuckne1@gmail.com>
2017-12-02 12:31:09 -0600

    New: Platform (fixes #26)

---
+++ game/platform.gd
@@ -0,0 +1,11 @@
+extends KinematicBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+#func _fixed_process(delta):
+#	move(Vector2(0, 1))
+
+#func _ready():
+#	set_fixed_process(true)
