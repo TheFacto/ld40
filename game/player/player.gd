5cfc1a2dc18ff39c4b2568c3fa24b8df8810efd2
stuckne1 <stuckne1@gmail.com>
2017-12-02 14:53:14 -0600

    Update: player jumps (fixes#13) (#42)

--- game/player/player.gd
+++ game/player/player.gd
@@ -4,15 +4,18 @@ extends KinematicBody2D
 const GRAVITY_VEC = Vector2(0,100)
 const WALK_SPEED = 100 # pixels/sec
 const FLOOR_NORMAL = Vector2(0,-1) # the direction of the ground
+const JUMP_SPEED = 125
 
 var linear_vel = Vector2()
+var jumping = false
 
 func _fixed_process(delta):
 	_gravity(delta)
 	_move_sideways(delta)
+	_jump(delta)
 	
 	# Commits the velocity to the kinematic body
-	move_and_slide(linear_vel, FLOOR_NORMAL)
+	linear_vel = move_and_slide(linear_vel, FLOOR_NORMAL)
 
 func _gravity(delta):
 	linear_vel += delta * GRAVITY_VEC
@@ -26,6 +29,14 @@ func _move_sideways(delta):
 		target_speed = 1
 	target_speed *= WALK_SPEED
 	linear_vel.x = lerp( linear_vel.x, target_speed, 0.1 )
+	
+func _jump(delta):
+	if (jumping and linear_vel.y > 0):
+		jumping = false
+	
+	if (Input.is_action_pressed("player_jump") and not jumping and is_move_and_slide_on_floor()):
+		linear_vel.y = -JUMP_SPEED
+		jumping = true	
 
 func _ready():
 	set_fixed_process(true)
