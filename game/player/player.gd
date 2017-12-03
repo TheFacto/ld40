853a6ba0a03c10222a5125ade89f9e37633f80bd
stuckne1 <stuckne1@gmail.com>
2017-12-02 18:02:22 -0600

    Steven strandee count movement slowdown (#49)
    * Update: player strandee slowdown fixes #17
    
    * Fix

--- game/player/player.gd
+++ game/player/player.gd
@@ -2,9 +2,12 @@ extends KinematicBody2D
 
 
 const GRAVITY_VEC = Vector2(0,100)
-const WALK_SPEED = 100 # pixels/sec
 const FLOOR_NORMAL = Vector2(0,-1) # the direction of the ground
-const JUMP_SPEED = 125
+
+export var walk_speed = 100 # pixels/sec
+export var min_walk_speed = 30
+export var jump_speed = 125
+export var min_jump_speed = 60
 
 var linear_vel = Vector2()
 var jumping = false
@@ -20,7 +23,7 @@ func _fixed_process(delta):
 
 	# Commits the velocity to the kinematic body
 	linear_vel = move_and_slide(linear_vel, FLOOR_NORMAL)
-
+	
 func _gravity(delta):
 	linear_vel += delta * GRAVITY_VEC
 
@@ -31,7 +34,7 @@ func _move_sideways(delta):
 		target_speed = -1
 	if (Input.is_action_pressed("player_right")):
 		target_speed = 1
-	target_speed *= WALK_SPEED
+	target_speed *= _get_weighted_player_velocity_x()
 	linear_vel.x = lerp( linear_vel.x, target_speed, 0.1 )
 
 func _jump(delta):
@@ -39,8 +42,8 @@ func _jump(delta):
 		jumping = false
 
 	if (Input.is_action_pressed("player_jump") and not jumping and is_move_and_slide_on_floor()):
-		linear_vel.y = -JUMP_SPEED
-		jumping = true
+		linear_vel.y = -_get_weighted_player_velocity_y()
+		jumping = true	
 
 func _ready():
 	set_fixed_process(true)
@@ -52,3 +55,17 @@ func capture_strandee():
 
 func _update_backpack():
 	get_node("Backpack/Sprite").set_frame(strandees + 1)
+
+func _get_weighted_player_velocity_x():
+	if (strandees == 0):
+		return walk_speed
+	else:
+		return max(walk_speed / strandees, min_walk_speed)
+		
+func _get_weighted_player_velocity_y():
+	if (strandees == 0):
+		return jump_speed
+	else:
+		return max(jump_speed  / strandees, min_jump_speed)
+
+	
